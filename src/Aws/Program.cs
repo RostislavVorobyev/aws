dfd64fa6e1060cf4a6cc2e1c3b40583b39880bd0
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-10-02 00:18:48 +0300

    Initial commit

---
+++ src/Aws/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace Aws
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new AwsStack(app, "AwsStack");
+
+            app.Synth();
+        }
+    }
+}
